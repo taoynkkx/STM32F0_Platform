d034f885af3fd47fd5e6901dcc16e28f3b1b8509
Ricky Tao <taoynkkx@163.com>
2018-08-25 00:42:49 +0800

    #10 improve print content, and update out

--- 020_Software/030_Projects/010_SelfTest/src/cfg/taskScheduler_cfg.c
+++ 020_Software/030_Projects/010_SelfTest/src/cfg/taskScheduler_cfg.c
@@ -11,24 +11,24 @@ extern unsigned long tick;
 void task_128ms(void)
 {
   mDIO_setDigitalOutput(DO_PB12, DO_CMD_TOGGLE);
-  xprintf("%d\t7\n", tick);
+  xprintf("%d\t\t+---\t|\t|\t|\n", tick);
 }
 
 void task_256ms(void)
 {
   mDIO_setDigitalOutput(DO_PB13, DO_CMD_TOGGLE);
-  xprintf("%d\t\t8\n", tick);
+  xprintf("%d\t\t|\t+---\t|\t|\n", tick);
 }
 
 void task_512ms(void)
 {
   mDIO_setDigitalOutput(DO_PB14, DO_CMD_TOGGLE);
-  xprintf("%d\t\t\t9\n", tick);
+  xprintf("%d\t\t|\t|\t+---\t|\n", tick);
 }
 
 void task_1024ms(void)
 {
   mDIO_setDigitalOutput(DO_PB15, DO_CMD_TOGGLE);
-  xprintf("%d\t\t\t\tA\n", tick);
+  xprintf("%d\t\t|\t|\t|\t+---\n", tick);
 }
 
